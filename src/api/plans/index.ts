db7254a0e0f3c779cf9b3c763fee4d4e774f884f
alekokhin <alekokhinikadze@gmail.com>
2024-12-09 10:33:11 +0400

    feat: environments

--- src/api/plans/index.ts
+++ src/api/plans/index.ts
@@ -2,7 +2,4 @@ import { TYPES } from 'constants/types'
 import { request } from 'lib/request'
 
 export const getPlans = () =>
-  // eslint-disable-next-line sonarjs/no-clear-text-protocols
-  request('http://192.168.100.14:8080/api/wizard/plans').get<Array<TYPES.Plan>>(
-    {},
-  )
+  request(`${process.env.REACT_APP_API_URL}/plans`).get<Array<TYPES.Plan>>({})
--- src/api/wizard/index.ts
+++ src/api/wizard/index.ts
@@ -1,12 +1,7 @@
-/* eslint-disable sonarjs/no-clear-text-protocols */
 import { request } from 'lib/request'
 
 export const sendWizard = async (body: any) => {
-  // eslint-disable-next-line no-console
-  console.log(body)
-  return request(
-    'http://192.168.100.14:8080/api/wizard/checkout ',
-  ).post<string>({
+  return request(`${process.env.REACT_APP_API_URL}/checkout`).post<string>({
     body,
     type: 'file',
   })
